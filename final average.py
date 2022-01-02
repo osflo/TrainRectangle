4c5a67680cfadb29dd1e48984ec3152d95574e71
osflo <48621374+osflo@users.noreply.github.com>
2022-01-02 18:29:31 +0100

    graph

---
+++ final average.py
@@ -0,0 +1,20 @@
+f=open('ratio_random_connected_components_after correction.txt','r')
+list_ratio=[]
+
+for line in f:
+    r=str()
+    for char in line:
+        if char==' 'or char=='\n':
+            if r!='':
+                list_ratio.append(float(r))
+                r=''
+        else:
+            r=r+char
+    if r!='':
+        list_ratio.append(float(r))
+
+f.close()
+
+avrg=sum(list_ratio)/len(list_ratio)
+print(avrg)
+print(len(list_ratio))
